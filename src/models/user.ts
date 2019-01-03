cc2a85cc154206202bc876b23610e912dea8b635
Sun1ive <sunliveua@gmail.com>
2019-01-04 00:01:16 +0200

    upd

--- src/models/user.ts
+++ src/models/user.ts
@@ -3,15 +3,37 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-  _id: Schema.Types.ObjectId,
   email: {
     type: String,
     required: true,
     unique: true
   },
   password: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
     type: String
   }
+});
+
+/**
+ * This property will ensure our virtuals (including "id")
+ * are set on the user when we use it.
+ */
+
+userSchema.set('toObject', { virtuals: true });
+
+/**
+ * This is a helper method which converts mongoose properties
+ * from objects to strings, numbers, and booleans.
+ */
+userSchema.method('toGraph', function toGraph(this: any) {
+  return JSON.parse(JSON.stringify(this));
 });
 
 export default model('User', userSchema);
