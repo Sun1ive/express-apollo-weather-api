036dded3abb896dc8a43c8dd66aca1f4ab87eea8
Sergey <sunliveua@gmail.com>
2019-01-04 23:16:44 +0200

    Delete config.js

--- dist/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PORT = process.env.PORT || 3001;
-exports.DB_USER = process.env.DB_USER;
-exports.DB_PASSWORD = process.env.DB_PASSWORD;
-//# sourceMappingURL=config.js.map
